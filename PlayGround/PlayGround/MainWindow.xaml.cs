b5abd9437357921e7991afeb929c58cf5e5fc8fe
ironicinc <seedeverltd@gmail.com>
2022-02-25 20:44:57 +0530

    App icon added

--- PlayGround/PlayGround/MainWindow.xaml.cs
+++ PlayGround/PlayGround/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace PlayGround
         public MainWindow()
         {
             InitializeComponent();
+            Icon = ImageAwesome.CreateImageSource(FontAwesomeIcon.SoccerBallOutline, Brushes.Green);
         }
     }
 }
