eccde6f861793f7a4d6d8e7fb667093efdbfdaaa
RevathiRajendranB <100304425+RevathiRajendranB@users.noreply.github.com>
2022-02-26 10:26:02 +0530

    UsersModel is created

---
+++ PlayGround/EntityLayer/UsersModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    /// <summary>
+    /// to store user data
+    /// </summary>
+    public class UsersModel
+    {
+        public int UserId { get; set;}
+        public string UserName { get; set;}
+        public string Name { get; set;}
+        public string UserEmailID { get; set;}
+        public string PhoneNumber { get; set;}
+        public string Password { get; set;}
+        public string ConfirmPassword { get; set;}
+        public int Status { get; set;}
+        public DateTime DateOfCreatedAccount { get; set;}
+        public  int RoleID { get; set;}
+        public string City { get; set;}
+        public string State { get; set;}
+        public string Zip { get; set;}
+        public string Avatar { get; set;}
+
+    }
+}
