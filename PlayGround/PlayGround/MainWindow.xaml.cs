d234c0895b1141064439d69c1afda928c8341b74
ironicinc <seedeverltd@gmail.com>
2022-03-02 22:23:49 +0530

    some modification in top menu

--- PlayGround/PlayGround/MainWindow.xaml.cs
+++ PlayGround/PlayGround/MainWindow.xaml.cs
@@ -48,6 +48,16 @@ namespace PlayGround
             }
             paletteHelper.SetTheme(theme);
         }
-        
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
     }
 }
