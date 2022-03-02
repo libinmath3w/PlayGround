5e14bdfcb1bdff64bf92e6121026569f0f2b3be2
ironicinc <seedeverltd@gmail.com>
2022-03-02 10:42:27 +0530

    main window  margin updaated and new user login class created

---
+++ PlayGround/PlayGround/ViewModel/UserLoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PlayGround.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PlayGround.ViewModel
+{
+    public class UserLoginViewModel : BaseViewModel
+    {
+        public ICommand UserLoginViewCommands { get; set; }
+        public UserLoginViewModel()
+        {
+
+        }  
+    }
+}
