a0a29515c9781a048ea722d6e47063ae68a10638
Libin Mathew <seedeverltd@gmail.com>
2022-03-03 21:04:51 +0530

    admin dashboard and some pages created

---
+++ PlayGround/PlayGround/View/AdminMainWindowView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using FontAwesome.WPF;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlayGround.View
+{
+    /// <summary>
+    /// Interaction logic for AdminMainWindowView.xaml
+    /// </summary>
+    public partial class AdminMainWindowView : Window
+    {
+        public bool IsDarknLightMode { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        public AdminMainWindowView()
+        {
+            InitializeComponent();
+            Icon = ImageAwesome.CreateImageSource(FontAwesomeIcon.SoccerBallOutline, Brushes.Green);
+            DataContext = new AdminMainWindowView();
+        }
+        private void TongleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarknLightMode = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarknLightMode = false;
+                theme.SetBaseTheme(Theme.Light);
+                //darkmodes.Content = "Dark Mode";
+            }
+            else
+            {
+                IsDarknLightMode = true;
+                theme.SetBaseTheme(Theme.Dark);
+                //darkmodes.Content = "Light Mode";
+            }
+            paletteHelper.SetTheme(theme);
+        }
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+    }
+}
