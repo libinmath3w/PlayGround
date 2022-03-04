8515985dabd095520048d64676fa5a01ce1ef625
SaranyaAVP <100303003+SaranyaAVP@users.noreply.github.com>
2022-03-05 01:31:53 +0530

    some user signup data removed

--- PlayGround/DataAccessLibrary/UserSignUpData.cs
+++ PlayGround/DataAccessLibrary/UserSignUpData.cs
@@ -16,17 +16,11 @@ namespace DataAccessLibrary
             TurfManagementDBEntities turfManagementDBEntities = new TurfManagementDBEntities();
             User user = new User();
             user.UserName = userModel.UserName;
-            user.ID = userModel.UserId;
             user.Name = userModel.Name;
             user.Email = userModel.UserEmailID;
             user.Password = userModel.Password;
             user.PhoneNumber = userModel.PhoneNumber;
-            user.Status = userModel.Status;
-            user.Role_ID = userModel.RoleID;
-            user.State = userModel.State;
-            user.City = userModel.City;
-            user.Zip = userModel.Zip;
-            user.Avatar = userModel.Avatar;
+            user.Role_ID = 2;
             user.Date_Of_Created_Account = userModel.DateOfCreatedAccount;
             turfManagementDBEntities.Users.Add(user);
             turfManagementDBEntities.SaveChanges();
