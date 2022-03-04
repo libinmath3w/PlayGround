a030455322b25f7f3bd8677b3dad4c15d0cb88a9
libinmath3w <musichevenindia@gmail.com>
2022-03-04 10:39:19 +0530

    userbooking page added

---
+++ PlayGround/DataAccessLibrary/UserBookingHistoryData.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer;
+using DataAccessLibrary;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class UserBookingHistoryData : IUserBookingHistory
+    {
+        public List<BookingModel> GetBookingDetails(BookingModel bookingModel)
+        {
+            List<BookingModel> BookingResultList = new List<BookingModel>();
+            try
+            {
+                TurfManagementDBEntities turfManagementDBEntities = new TurfManagementDBEntities();
+                var query = from bookings in turfManagementDBEntities.Bookings
+                            where bookings.User_ID.Equals(bookingModel.UserID)
+                            select bookings;
+                foreach (var item in query)
+                {
+                    BookingModel bookingModels = new BookingModel();
+                    bookingModels.UserID = item.User_ID;
+                    bookingModels.BookingID = item.Booking_ID;
+                    bookingModels.BookingDate = (DateTime)item.Booking_Date;
+                    bookingModels.TurfID = item.Turf_ID;
+                    bookingModels.StartTime = item.Start_Time;
+                    bookingModels.EndTime = item.End_Time;
+                    bookingModels.Amount = (float)item.Amount;
+                    bookingModels.BookingStatus = (bool)item.Booking_Status;
+                    bookingModels.PaymentStatus = item.Payment_Status;
+                    BookingResultList.Add(bookingModels);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return BookingResultList;
+        }
+    }
+}
--- PlayGround/PlayGround/ViewModel/UserTurfBookingViewModel.cs
+++ PlayGround/PlayGround/ViewModel/UserTurfBookingViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer;
+using EntityLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,13 @@ namespace PlayGround.ViewModel
 {
     public class UserTurfBookingViewModel : BaseViewModel
     {
+        private List<BookingModel> _viewUserTurfBokkingsAndBookNew;
+        public List<BookingModel> ViewUserTurfBokkingsAndBookNew { get => _viewUserTurfBokkingsAndBookNew; set => _viewUserTurfBokkingsAndBookNew = value; }
+        public UserBookingHistoryBusinessModel userBookingHistoryBusinessModel = new UserBookingHistoryBusinessModel();
+        public UserTurfBookingViewModel(BookingModel bookingModel)
+        {
+            bookingModel.UserID = 2;
+            _viewUserTurfBokkingsAndBookNew = userBookingHistoryBusinessModel.GetBookingDetails(bookingModel);
+        }
     }
 }
