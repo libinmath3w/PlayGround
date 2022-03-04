f83050c9a2ec0bf5e02f2ba435572c4c42d5f256
libinmath3w <musichevenindia@gmail.com>
2022-03-04 15:41:06 +0530

    admin navigation added

---
+++ PlayGround/PlayGround/Commands/AdminRedirectViewCommand.cs
@@ -0,0 +1,40 @@
+﻿using EntityLayer;
+using PlayGround.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PlayGround.Commands
+{
+    public class AdminRedirectViewCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public AdminMainWindowViewModel viewModel { get; set; }
+        public AdminRedirectViewCommand(AdminMainWindowViewModel adminMainWindowViewModel)
+        {
+            this.viewModel = adminMainWindowViewModel;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "AdminDashboard")
+            {
+                viewModel.SelectedViewModel = new AdminUserDashboardViewModel();
+            }
+            else if (parameter.ToString() == "AdminSettings")
+            {
+                viewModel.SelectedViewModel = new AdminSettingsViewModel();
+            }
+            else if (parameter.ToString() == "AdminTurfBookingDetails")
+            {
+                viewModel.SelectedViewModel = new AdminTurfBookingHistoryViewModel();
+            }
+        }
+    }
+}
--- PlayGround/PlayGround/ViewModel/AdminMainWindowViewModel.cs
+++ PlayGround/PlayGround/ViewModel/AdminMainWindowViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using FontAwesome.WPF;
 using MaterialDesignThemes.Wpf;
+using PlayGround.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace PlayGround.ViewModel
 {
     public class AdminMainWindowViewModel : BaseViewModel
     {
-        
+        private BaseViewModel _selectedViewModel = new AdminUserDashboardViewModel();
+        public BaseViewModel SelectedViewModel { get => _selectedViewModel; set { _selectedViewModel = value; onPropertyChanged(nameof(SelectedViewModel)); } }
+        public ICommand AdminRedirectViewCommands { get; set; }
+        public AdminMainWindowViewModel()
+        {
+            AdminRedirectViewCommands = new AdminRedirectViewCommand(this);
+        }
     }
 }
