5210e8207f956d9cba7de5dd50ab8aab3040db2f
VINAIRAJ1998 <96517318+VINAIRAJ1998@users.noreply.github.com>
2022-03-05 16:47:49 +0530

    AdminSetting Database Binding

---
+++ PlayGround/BusinessLayer/AdminSettingsBusinessModel.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLibrary;
+using EntityLayer;
+using EntityLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AdminSettingsBusinessModel : IUserSettings
+    {
+        public List<UsersModel> GetUserDetails(UsersModel usersModel)
+        {
+            AdminSettingsData usersettingsdata = new AdminSettingsData();
+            return usersettingsdata.GetUserDetails(usersModel);
+        }
+
+        public void SaveUserDetails(UsersModel usersModel)
+        {
+            AdminSettingsData usersettingsdata = new AdminSettingsData();
+            usersettingsdata.SaveUserDetails(usersModel);
+        }
+    }
+}
