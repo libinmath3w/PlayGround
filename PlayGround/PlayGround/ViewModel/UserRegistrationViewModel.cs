19f96b438d30687c07849eb9d1e32e0709d167f5
libinmath3w <musichevenindia@gmail.com>
2022-03-05 15:19:12 +0530

    added username field

--- PlayGround/PlayGround/ViewModel/UserRegistrationViewModel.cs
+++ PlayGround/PlayGround/ViewModel/UserRegistrationViewModel.cs
@@ -8,6 +8,11 @@ namespace PlayGround.ViewModel
 {
     public class UserRegistrationViewModel : BaseViewModel
     {
-
+        private string _username;
+        public string UserName { get => _username; set { _username = value; onPropertyChanged("User Name"); } }
+        public string Name { get; set; }
+        public string UserEmailID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
     }
 }
