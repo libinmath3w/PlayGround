38174a461e049d8f753e15331ccafe305b935678
RevathiRajendranB <100304425+RevathiRajendranB@users.noreply.github.com>
2022-03-05 15:27:38 +0530

    UserRegisterationViewModel is completed

--- PlayGround/PlayGround/ViewModel/UserRegistrationViewModel.cs
+++ PlayGround/PlayGround/ViewModel/UserRegistrationViewModel.cs
@@ -9,10 +9,14 @@ namespace PlayGround.ViewModel
     public class UserRegistrationViewModel : BaseViewModel
     {
         private string _username;
+        private string _name;
+        private string _userEmailID;
+        private string _phoneNumber;
+        private string _password;
         public string UserName { get => _username; set { _username = value; onPropertyChanged("User Name"); } }
-        public string Name { get; set; }
-        public string UserEmailID { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        public string Name { get => _name; set { _name = value; onPropertyChanged(" Name"); } }
+        public string UserEmailID { get => _userEmailID; set { _userEmailID = value; onPropertyChanged("User EmailID"); } }
+        public string PhoneNumber { get => _phoneNumber; set { _phoneNumber = value; onPropertyChanged("Phone Number"); } }
+        public string Password { get => _password; set { _password = value; onPropertyChanged("Password"); } }
     }
 }
