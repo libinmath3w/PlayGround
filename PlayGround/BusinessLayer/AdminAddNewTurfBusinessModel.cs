872445500004b39af52dd5f342f0d7564620718b
libinmath3w <musichevenindia@gmail.com>
2022-03-06 12:25:25 +0530

    add new turf classes created

---
+++ PlayGround/BusinessLayer/AdminAddNewTurfBusinessModel.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer;
+using EntityLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AdminAddNewTurfBusinessModel : IAddNewTurf
+    {
+        public void SaveNewTurf(TurfModel turfModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlayGround/DataAccessLibrary/AddNewTurfDataModel.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer;
+using EntityLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class AddNewTurfDataModel : IAddNewTurf
+    {
+        public void SaveNewTurf(TurfModel turfModel)
+        {
+            
+        }
+    }
+}
