27aa4a01ccc0e3709fb5a5cb91bfd5a1e0ddf1ba
libinmath3w <musichevenindia@gmail.com>
2022-03-09 10:31:46 +0530

    admin users details

---
+++ PlayGround/BusinessLayer/AdminUsersDetailsBusinessModel.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLibrary;
+using EntityLayer;
+using EntityLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AdminUsersDetailsBusinessModel : IAdminUsersDetails
+    {
+        AdminViewUserDetailsData adminViewUserDetailsData = new AdminViewUserDetailsData();
+        public List<UsersModel> GetAdminUsersDetails()
+        {
+            return adminViewUserDetailsData.GetAdminUsersDetails();
+        }
+    }
+}
