88b067692973ce0efb2a852f6343cb2671f6dbc4
VINAIRAJ1998 <96517318+VINAIRAJ1998@users.noreply.github.com>
2022-03-13 15:20:15 +0530

    AdminDashboard Design

---
+++ PlayGround/DataAccessLibrary/AdminDashboardData.cs
@@ -0,0 +1,44 @@
+﻿using EntityLayer;
+using EntityLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class AdminDashboardData : IAdminDashboard
+    {
+        public List<TurfModel> GetCounts(TurfModel turfModel)
+        {
+
+            try
+            {
+                List<TurfModel> Counts = new List<TurfModel>();
+                TurfModel turf = new TurfModel();
+                TurfManagementDBEntities dBEntities = new TurfManagementDBEntities();
+                var turfquery = (from turfCount in dBEntities.Turfs
+                                 select turfCount).Count();
+                turf.Total_turf_count = turfquery.ToString();
+
+                var userquery = (from userCount in dBEntities.Users
+                                 select userCount).Count();
+                turf.Total_users_count = userquery.ToString();
+
+                var bookingquery = (from bookingCount in dBEntities.Bookings
+                                    select bookingCount).Count();
+                turf.Total_booking_count = bookingquery.ToString();
+
+                Counts.Add(turf);
+                return Counts;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
--- PlayGround/PlayGround/ViewModel/AdminDashboardViewModel.cs
+++ PlayGround/PlayGround/ViewModel/AdminDashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer;
+using EntityLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,30 @@ namespace PlayGround.ViewModel
 {
     public class AdminDashboardViewModel : BaseViewModel
     {
+        private string _countTurf { get; set; }
+        public string CountTurf { get => _countTurf; set { _countTurf = value; onPropertyChanged(nameof(CountTurf)); } }
+        private string _countuser { get; set; }
+        public string CountUser { get => _countuser; set { _countuser = value; onPropertyChanged(nameof(CountUser)); } }
+        private string _countBooking { get; set; }
+        public string CountBooking { get => _countBooking; set { _countBooking = value; onPropertyChanged(nameof(CountBooking)); } }
+
+        public AdminDashboardViewModel()
+        {
+            GetAllCountData();
+        }
+
+        private void GetAllCountData()
+        {
+            AdminDashboardBusinessLayer adminDashboardBusiness = new AdminDashboardBusinessLayer();
+            TurfModel turfModel = new TurfModel();
+            var query = adminDashboardBusiness.GetCounts(turfModel);
+            foreach (var item in query)
+            {
+                this.CountTurf = item.Total_turf_count;
+                this.CountBooking = item.Total_booking_count;
+                this.CountUser = item.Total_users_count;
+            }
+
+        }
     }
 }
